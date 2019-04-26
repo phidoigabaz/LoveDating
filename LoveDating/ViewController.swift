8d3c556f8fcf7e3c12edb32c027176bb37c99721
Tạ Thành Đạt <dattt@gemvietnam.com>
2019-04-26 13:13:58 +0700

    Initial Commit

---
+++ LoveDating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoveDating
+//
+//  Created by Tạ Thành Đạt on 4/26/19.
+//  Copyright © 2019 dattt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
